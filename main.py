f4fc1457132f88c76c5e329b7764885ca3be3d7e
Madu <144852939+itsmaddu@users.noreply.github.com>
2026-04-17 23:49:41 +0000

    fix: implement model redundancy and update hub libraries

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-from transformers import pipeline
 import os
+from transformers import pipeline
 
-DIRETORIO_TEMP = "temp_audio"
+
+token_hf = os.getenv("HF_TOKEN")
 
 def classificar_genero(caminho_audio):
-    # Lista de modelos estáveis (se um falhar, tenta o outro)
-    modelos = [
-        "superb/wav2vec2-base-superb-gtzan", 
-        "anton-l/wav2vec2-base-superb-gtzan"
-    ]
-    
-    ultimo_erro = ""
-    
-    for nome_modelo in modelos:
-        try:
-            # O 'trust_remote_code' ajuda em alguns servidores
-            pipe = pipeline("audio-classification", model=nome_modelo)
-            resultados = pipe(caminho_audio, top_k=None)
-            return {"sucesso": True, "dados": resultados}
-        except Exception as e:
-            ultimo_erro = str(e)
-            continue # Tenta o próximo modelo da lista
-            
-    return {"sucesso": False, "erro": f"Nenhum modelo carregou. Erro: {ultimo_erro}"}
+    try:
+        
+        pipe = pipeline(
+            "audio-classification", 
+            model="superb/wav2vec2-base-superb-gtzan",
+            token=token_hf
+        )
+        
+        resultados = pipe(caminho_audio, top_k=None)
+        return {"sucesso": True, "dados": resultados}
+        
+    except Exception as e:
+        return {"sucesso": False, "erro": str(e)}
