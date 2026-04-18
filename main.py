3ae2efb750a4d9990c956efe5f9e206564a1ed1b
Madu <144852939+itsmaddu@users.noreply.github.com>
2026-04-18 21:55:11 +0000

    feat: switch to MIT AST model and add torchaudio

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import os
+import torch
 from transformers import pipeline
 
-# Adicionamos isto de volta para manter o código limpo
-DIRETORIO_TEMP = "temp_audio"
 
-# Esta linha vai buscar o Token que você salvou nos Secrets do Space
-token_hf = os.getenv("HF_TOKEN")
+DIRETORIO_TEMP = "temp_audio"
 
 def classificar_genero(caminho_audio):
     try:
-        # Usamos o modelo 'anton-l' que é mais leve e estável para Spaces
+        
+        token_hf = os.getenv("HF_TOKEN")
+        
         pipe = pipeline(
             "audio-classification", 
-            model="anton-l/wav2vec2-base-superb-gtzan",
+            model="MIT/ast-finetuned-gtzan",
             token=token_hf
         )
         
         resultados = pipe(caminho_audio, top_k=None)
-        return {"sucesso": True, "dados": resultados}
+        
+        return {
+            "sucesso": True,
+            "dados": resultados
+        }
         
     except Exception as e:
+        
         return {"sucesso": False, "erro": str(e)}
