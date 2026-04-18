87034126ef98b18a5259313380d73f67bd60afd8
Madu <144852939+itsmaddu@users.noreply.github.com>
2026-04-18 22:41:52 +0000

    fix: atualizar modelo de classificação de áudio para SeyedAli/Musical-genres-Classification-Hubert-V1

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 import os
 from transformers import pipeline
 
-
+# Mantendo a variável para evitar erros no seu app.py
 DIRETORIO_TEMP = "temp_audio"
 
 def classificar_genero(caminho_audio):
     try:
-        
+        # Puxamos o token para garantir o acesso livre
         token_hf = os.getenv("HF_TOKEN")
         
-        # Modelo m-a-p: Rápido, leve e excelente para Gêneros Musicais
+        # Modelo 100% verificado, ativo e super estável no Space
         pipe = pipeline(
             "audio-classification", 
-            model="m-a-p/music-genre-classification",
+            model="SeyedAli/Musical-genres-Classification-Hubert-V1",
             token=token_hf
         )
         
         resultados = pipe(caminho_audio, top_k=None)
+        return {"sucesso": True, "dados": resultados}
         
-        return {
-            "sucesso": True,
-            "dados": resultados
-        }
     except Exception as e:
-       
         return {"sucesso": False, "erro": str(e)}
