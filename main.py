e52731e61959854e174a0b57d8ee4492424c5f8a
Madu <144852939+itsmaddu@users.noreply.github.com>
2026-04-18 00:18:35 +0000

    fix: use light model and prepare for factory reboot

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
 from transformers import pipeline
 
+# Adicionamos isto de volta para manter o código limpo
+DIRETORIO_TEMP = "temp_audio"
 
+# Esta linha vai buscar o Token que você salvou nos Secrets do Space
 token_hf = os.getenv("HF_TOKEN")
 
 def classificar_genero(caminho_audio):
     try:
-        
+        # Usamos o modelo 'anton-l' que é mais leve e estável para Spaces
         pipe = pipeline(
             "audio-classification", 
-            model="superb/wav2vec2-base-superb-gtzan",
+            model="anton-l/wav2vec2-base-superb-gtzan",
             token=token_hf
         )
         
